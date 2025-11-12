90948801d8f00bd0fa799c6888f3a60259645fa1
Leonard Tan <lentan1029@gmail.com>
2025-11-12 18:05:25 +0800

    changed value to string for metadata, changed naming of key and value to metadataKey and metadataValue, changed tokenUri to agentUri

--- contracts/IdentityRegistryUpgradeable.sol
+++ contracts/IdentityRegistryUpgradeable.sol
@@ -15,15 +15,15 @@ contract IdentityRegistryUpgradeable is
     uint256 private _lastId;
 
     // agentId => key => value
-    mapping(uint256 => mapping(string => bytes)) private _metadata;
+    mapping(uint256 => mapping(string => string)) private _metadata;
 
     struct MetadataEntry {
-        string key;
-        bytes value;
+        string metadataKey;
+        string metadataValue;
     }
 
-    event Registered(uint256 indexed agentId, string tokenURI, address indexed owner);
-    event MetadataSet(uint256 indexed agentId, string indexed indexedKey, string key, bytes value);
+    event Registered(uint256 indexed agentId, string agentUri, address indexed owner);
+    event MetadataSet(uint256 indexed agentId, string indexed indexedMetadataKey, string metadataKey, string metadataValue);
     event UriUpdated(uint256 indexed agentId, string newUri, address indexed updatedBy);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -59,16 +59,16 @@ contract IdentityRegistryUpgradeable is
         emit Registered(agentId, tokenUri, msg.sender);
 
         for (uint256 i = 0; i < metadata.length; i++) {
-            _metadata[agentId][metadata[i].key] = metadata[i].value;
-            emit MetadataSet(agentId, metadata[i].key, metadata[i].key, metadata[i].value);
+            _metadata[agentId][metadata[i].metadataKey] = metadata[i].metadataValue;
+            emit MetadataSet(agentId, metadata[i].metadataKey, metadata[i].metadataKey, metadata[i].metadataValue);
         }
     }
 
-    function getMetadata(uint256 agentId, string memory key) external view returns (bytes memory) {
+    function getMetadata(uint256 agentId, string memory key) external view returns (string memory) {
         return _metadata[agentId][key];
     }
 
-    function setMetadata(uint256 agentId, string memory key, bytes memory value) external {
+    function setMetadata(uint256 agentId, string memory key, string memory value) external {
         require(
             msg.sender == _ownerOf(agentId) ||
             isApprovedForAll(_ownerOf(agentId), msg.sender) ||
